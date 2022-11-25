4ee675a4ef1056d9c989cfabeed7432121b5b66d
Timo <developer.timo@gmail.com>
2022-11-25 16:13:14 +0900

    배치 작업의 기본, 파일 읽기와 쓰기
    - CSV 파일 읽기 테스트

---
+++ src/main/java/com/example/SpringBatchTutorial/job/filedatareadwrite/FileDataReadWriterConfig.java
@@ -0,0 +1,60 @@
+package com.example.SpringBatchTutorial.job.filedatareadwrite;
+
+import com.example.SpringBatchTutorial.job.filedatareadwrite.dto.Player;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * desc: 파일 읽고 쓰기
+ * run: --spring.batch.job.names=fileReadWriteJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class FileDataReadWriterConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job fileReadWriteJob(Step fileReadWriteStep) {
+        return jobBuilderFactory.get("fileReadWriteJob")
+                .incrementer(new RunIdIncrementer())
+                .start(fileReadWriteStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step fileReadWriteStep(ItemReader<Player> playerItemReader) {
+        return stepBuilderFactory.get("fileReadWriteStep")
+                .<Player, Player>chunk(5)
+                .reader(playerItemReader)
+                .writer(items -> items.forEach(System.out::println))
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public FlatFileItemReader<Player> playerItemReader() {
+        return new FlatFileItemReaderBuilder<Player>()
+                .name("playerItemReader")
+                .resource(new FileSystemResource("Players.csv"))
+                .lineTokenizer(new DelimitedLineTokenizer())
+                .fieldSetMapper(new PlayerFieldSetMapper())
+                .linesToSkip(1)
+                .build();
+    }
+}
