ca08f983c77954a7d5c081616d8e0b21fea26838
Timo <developer.timo@gmail.com>
2022-11-25 15:12:21 +0900

    DB 데이터 이관 하기 (DB 데이터 읽고 쓰기)
    - Reader 테스트

---
+++ src/main/java/com/example/SpringBatchTutorial/job/dbdatareadwrite/TrMigrationConfig.java
@@ -0,0 +1,66 @@
+package com.example.SpringBatchTutorial.job.dbdatareadwrite;
+
+import com.example.SpringBatchTutorial.core.domain.accounts.AccountsRepository;
+import com.example.SpringBatchTutorial.core.domain.orders.Orders;
+import com.example.SpringBatchTutorial.core.domain.orders.OrdersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import java.util.Collections;
+
+/**
+ * desc: 주문 테이블 -> 정산 테이블 데이터 이관
+ * run: --spring.batch.job.names=trMigrationJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class TrMigrationConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final OrdersRepository ordersRepository;
+    private final AccountsRepository accountsRepository;
+
+    @Bean
+    public Job trMigrationJob(Step trMigrationStep) {
+        return jobBuilderFactory.get("trMigrationJob")
+                .incrementer(new RunIdIncrementer())
+                .start(trMigrationStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step trMigrationStep(ItemReader<Orders> trOrdersReader) {
+        return stepBuilderFactory.get("trMigrationStep")
+                .<Orders, Orders>chunk(5)
+                .reader(trOrdersReader)
+                .writer(items -> items.forEach(System.out::println))
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public RepositoryItemReader<Orders> trOrdersReader() {
+        return new RepositoryItemReaderBuilder<Orders>()
+                .name("trOrdersReader")
+                .repository(ordersRepository)
+                .methodName("findAll")
+                .pageSize(5)
+                .arguments(Collections.emptyList())
+                .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
+                .build();
+    }
+}
